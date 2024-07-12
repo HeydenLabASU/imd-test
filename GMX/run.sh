653de69241657cecc303ce67b3e7e02d147b766a
Matthias Heyden <mheyden1@asu.edu>
2024-07-11 17:50:25 -0700

    Update run.sh

--- GMX/run.sh
+++ GMX/run.sh
@@ -2,6 +2,6 @@
 
 rm run.cpt run.gro run.trr run.edr run.log run.tpr run.xvg imd.gro mdout.mdp
 
-gmx grompp -f run.mdp -c strcut.gro -p topol.top -imd imd.gro -o run.tpr
+gmx grompp -f run.mdp -c struct.gro -p topol.top -imd imd.gro -o run.tpr
 
 gmx mdrun -nt 1 -deffnm run -imdport 8888 -imdwait
